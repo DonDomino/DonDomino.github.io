cbe6faaa3c4b138dccef2d25a1d7fca847707033
Mauricio Orozco <maudob@live.com>
2019-09-18 16:09:15 -0500

    pre-deploy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const contentLoad = (async () => {
 
 	const loadTestimonials = async (url) => {
-
 		try {
 			let response = await fetch(url);
 			let data = await response.json();
@@ -10,58 +9,37 @@ const contentLoad = (async () => {
 		catch(err) {
 			console.error(err);
 		}
-	}
+  }
+  
+  const render = (userName, postBody, pics) => {
+    let newItem = document.createElement('div');
+    newItem.classList.add('carousel-item');
+    newItem.innerHTML = `
+      <div class="testimonial-wrapper">
+        <div class="testimonial-pic">
+          <img src="${pics}" alt="">
+        </div>
+        <p>"${postBody}"</p>
+        <span>${userName}</span>
+      </div>
+    `;
+    
+    let toAppend = document.querySelector('.carousel-inner');
+    toAppend.appendChild(newItem);
+  }
 
 	const users = await loadTestimonials("https://jsonplaceholder.typicode.com/users");
 	const posts = await loadTestimonials("https://jsonplaceholder.typicode.com/posts");
-	const pics = [ "./images/person_1.jpg", "./images/person_2.jpg", "./images/person_3.jpg", "./images/person_4.jpg" ];
+  const pics = [ "./images/person_1.jpg", "./images/person_2.jpg", "./images/person_3.jpg", "./images/person_4.jpg" ];
 
-  const testimonialPics = pics.map( pic => `
-    <div class="testimonial">
-      <img src="${pic}" class="d-block w-100" alt="">
-    </div>` 
-  );
+  for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      if(posts[i*10].userId === users[j].id){
+        render(users[j].name, posts[j*10].body, pics[j]);
+      }  
+    }      
+  }
 
-  const postBody = posts.map( (post, index) => `<p>${post.userId[index].body}</p>` ).splice(0,4);
-  const userName = users.map( user => `<span>${user.name}</span>`).splice(0,4);
-  console.log(postBody);
-  // for(let i = 1; i < 4; i++){
-  //   const render = () => {
-  //     return(`
-  //       <div class="carousel-item">
-  //         <div class="testimonial">
-  //           <img src="${pics[i]}" class="d-block w-100" alt="">
-  //         </div>
-  //         <p>${posts[i].body}</p>
-  //         <span>${users[i].name}</span>
-  //       </div>        
-  //     `)
-  //   }    
-  //   document.getElementsByClassName('carousel-inner')[i].innerHTML = render();
-  // }  
+  document.querySelector('.carousel-inner').firstElementChild.classList.add('active');
 
 })();
-
-// for(let i = 1; i < pics.length; i++){
-//   const render = () => {
-//     i = 1 ? 
-//       `<div class="carousel-item active">
-//         <div class="testimonial">
-//           <img src="${pics[i]}" class="d-block w-100" alt="">
-//         </div>
-//         <p>${posts[i].body}</p>
-//         <span>${users[i].name}</span>
-//       </div>`
-//       :
-//       `<div class="carousel-item">
-//         <div class="testimonial">
-//           <img src="${pics[i]}" class="d-block w-100" alt="">
-//         </div>
-//         <p>${posts[i].body}</p>
-//         <span>${users[i].name}</span>
-//       </div>`
-    
-//   }    
-//   document.querySelector('.carousel-inner')[i].innerHTML = render();
-// }  
-
